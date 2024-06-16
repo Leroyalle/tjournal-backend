c9ecd997f91b0bc43bb965ed7644b8f9dadcf9ad
Leroyalle <nikolajmelonov@yandex.ru>
2024-06-16 22:00:40 +0300

    аутентификация через jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UserService } from 'src/user/user.service';
-
+import { UserEntity } from 'src/user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findByCond({
@@ -16,5 +18,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user: UserEntity) {
+    const { password, ...userData } = user;
+    const payload = { email: user.email, sub: user.id };
+    return {
+      ...userData,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'test',
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const data = { id: payload.sub, email: payload.email };
+    const user = await this.userService.findById(data.id);
+    if (!user) {
+      throw new UnauthorizedException('Нет доступа к этой странице');
+    }
+    return data;
+  }
+}
